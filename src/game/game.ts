0cb257923d3a2c0a322d0b0cbb7b6777202b2b3f
Alejandro Seguí <alesegdia@gmail.com>
2017-02-12 18:35:48 +0100

    working on game

--- src/game/game.ts
+++ src/game/game.ts
@@ -1,7 +1,24 @@
 /// <reference path="../libs/phaser/typescript/phaser.d.ts" />
 
 class MyGame {
-constructor() {
-		var met = new Meh("1");
+
+	constructor() {
+		this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create } );
 	}
+
+	game: Phaser.Game;
+
+	preload() {
+		this.game.load.image('ball', 'assets/img/ball.png');
+	}
+
+	create() {
+		var ball = this.game.add.sprite( this.game.world.centerX, this.game.world.centerY, 'ball' );
+		ball.anchor.setTo(0.5, 0.5);
+	}
+
+}
+
+window.onload = () => {
+	var game = new MyGame();
 }
